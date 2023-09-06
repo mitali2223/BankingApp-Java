67137e80548466df8621d0b92d79f709444f8bfa
Mitali Gupta <mitaligupta2223@gmail.com>
2023-09-06 20:29:19 +0530

    last

--- src/banking_app/Banking.java
+++ src/banking_app/Banking.java
@@ -1,30 +1,48 @@
 package banking_app;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Banking {
-	float balance;
-	public Banking(){
-
+	Database_Service db_service;
+	ResultSet res;
+	public Banking(Database_Service db_Service ){
+		this.db_service = db_Service;
 	}
-	
-	public void addMoney(int amount) {
-		balance += amount;
-		
-		System.out.println(balance);
-
-		
+	public void addMoney(int amount, String username) throws SQLException {
+		db_service.dbAddMoney(amount,username);	
+		res = db_service.fetchData(username);
+		while(res.next()){
+			System.out.println(res.getInt(1) + " " + res.getString(2)+ " " + res.getFloat(3));;
+		}	
 	}
-	public void withdrawMoney() {
-		
+	public void withdrawMoney(int amount, String username , String password) throws SQLException {
+		res = db_service.fetchData(username);
+	    while(res.next()){
+			System.out.println(res.getInt(1) + " " + res.getString(2)+ " " + res.getFloat(3));
+			if(res.getInt(3) >= amount){
+				db_service.dbWithdrawMoney(amount, username, password);
+			}else{
+				System.out.println("insufficient balance");
+			}
+		}
 	}
-	public void transferMoney() {
-		
+	public void transferMoney(int account_no , int amount, String username , String password) throws SQLException {
+		res = db_service.fetchData(username);
+	    while(res.next()){
+			
+			if(res.getInt(3) >= amount){
+				db_service.dbTransferMoney(account_no, amount, username, password);
+		}else{
+			System.out.println("insufficient balance");
+		}
 	}
-	public void checkAccountBalance() {
-		
+			System.out.println("transaction successfully completed");
 	}
-
-	
-	
-	
+	public void checkAccountBalance(String username) throws SQLException {
+	    res =  db_service.fetchData(username);
+		while(res.next()){
+			System.out.println( "Account balance : " + res.getFloat(3));;
+		}
+	} 	
 }
