ea20795087c988d6b164d8afee8b2d639d7650f8
Mitali Gupta <mitaligupta2223@gmail.com>
2023-09-06 20:56:49 +0530

    hdfgd

--- src/banking_app/Banking.java
+++ src/banking_app/Banking.java
@@ -13,15 +13,15 @@ public class Banking {
 		db_service.dbAddMoney(amount,username);	
 		res = db_service.fetchData(username);
 		while(res.next()){
-			System.out.println(res.getInt(1) + " " + res.getString(2)+ " " + res.getFloat(3));;
+				System.out.println( "\nBalance : " + res.getFloat(3));
 		}	
 	}
 	public void withdrawMoney(int amount, String username , String password) throws SQLException {
 		res = db_service.fetchData(username);
 	    while(res.next()){
-			System.out.println(res.getInt(1) + " " + res.getString(2)+ " " + res.getFloat(3));
 			if(res.getInt(3) >= amount){
 				db_service.dbWithdrawMoney(amount, username, password);
+				System.out.println( "\nBalance : " + res.getFloat(3));
 			}else{
 				System.out.println("insufficient balance");
 			}
@@ -29,10 +29,10 @@ public class Banking {
 	}
 	public void transferMoney(int account_no , int amount, String username , String password) throws SQLException {
 		res = db_service.fetchData(username);
-	    while(res.next()){
-			
+	    while(res.next()){	
 			if(res.getInt(3) >= amount){
 				db_service.dbTransferMoney(account_no, amount, username, password);
+				System.out.println( "\nBalance : " + res.getFloat(3));
 		}else{
 			System.out.println("insufficient balance");
 		}
@@ -42,7 +42,7 @@ public class Banking {
 	public void checkAccountBalance(String username) throws SQLException {
 	    res =  db_service.fetchData(username);
 		while(res.next()){
-			System.out.println( "Account balance : " + res.getFloat(3));;
+			System.out.println("\nAccount No : " + res.getInt(1) +  "\nBalance : " + res.getFloat(3));
 		}
 	} 	
 }
